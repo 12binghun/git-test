2a631d4d8c3a75d285c584d2282bf4554a4aa508
Suao <825037759@qq.com>
2022-04-18 22:42:56 +0800

    first commit

---
+++ src/main/java/com/suao/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.suao.git;
+
+/**
+ * @Auther: [冰魂]
+ * @Date: 2022/4/18 - 04 - 18 - 22:39
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
